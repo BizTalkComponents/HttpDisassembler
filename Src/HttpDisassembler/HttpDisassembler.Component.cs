dfcccae8b10e25cf455d3f067a8927d82ae2addf
robinhultman <robin.hultman@gmail.com>
2014-10-17 11:06:33 +0200

    Implemented component

---
+++ Src/HttpDisassembler/HttpDisassembler.Component.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace BizTalkComponents.PipelineComponents.HttpDisassembler
+{
+    public partial class HttpDisassembler
+    {
+        public string Name { get; private set; }
+        public string Version { get; private set; }
+        public string Description { get; private set; }
+        public void GetClassID(out Guid classID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitNew()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IntPtr Icon { get; private set; }
+    }
+}
---
+++ Src/HttpDisassembler/HttpDisassembler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using BizTalkComponents.Utils.ContextPropertyHelpers;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+using Microsoft.XLANGs.RuntimeTypes;
+
+namespace BizTalkComponents.PipelineComponents.HttpDisassembler
+{
+    [System.Runtime.InteropServices.Guid("FE75A97A-EB7C-49AF-8778-136FA366A5F4")]
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [ComponentCategory(CategoryTypes.CATID_DisassemblingParser)]
+    public partial class HttpDisassembler : IBaseComponent, IPersistPropertyBag, IComponentUI, IDisassemblerComponent
+    {
+        public void Load(IPropertyBag propertyBag, int errorLog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Disassemble(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+
+            //Get a reference to the BizTalk schema.
+            var documentSpec = (DocumentSpec)pContext.GetDocumentSpecByName(DocumentSpecName);
+
+            //Get a list of properties defined in the schema.
+            var annotations = documentSpec.GetPropertyAnnotationEnumerator();
+            var doc = new XmlDocument();
+            var sw = new StringWriter(new StringBuilder());
+
+            //Create a new instance of the schema.
+            doc.Load(documentSpec.CreateXmlInstance(sw));
+            sw.Dispose();
+
+            //Write all properties to the message body.
+            while (annotations.MoveNext())
+            {
+                var annotation = (IPropertyAnnotation)annotations.Current;
+                var node = doc.SelectSingleNode(annotation.XPath);
+                string propertyValue;
+
+                if (pInMsg.Context.TryRead(new ContextProperty(annotation.Name, annotation.Namespace),out propertyValue))
+                {
+                    node.InnerText = propertyValue;
+                }
+            }
+
+            var ms = new MemoryStream();
+            doc.Save(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var outMsg = pInMsg;
+            outMsg.BodyPart.Data = ms;
+
+            //Promote message type and SchemaStrongName
+            outMsg.Context.Promote(new ContextProperty(SystemProperties.MessageType), documentSpec.DocType);
+            outMsg.Context.Promote(new ContextProperty(SystemProperties.SchemaStrongName), documentSpec.DocSpecStrongName);
+
+            _outputQueue.Enqueue(outMsg);
+
+        }
+
+        public IBaseMessage GetNext(IPipelineContext pContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
