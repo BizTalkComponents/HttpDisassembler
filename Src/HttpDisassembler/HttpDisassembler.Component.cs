e1058c35c55a372a4b54c9499702ae4a6161e7d0
robinhultman <robin.hultman@gmail.com>
2014-11-10 14:50:26 +0100

    Fixed invalid metadata properties

--- Src/HttpDisassembler/HttpDisassembler.Component.cs
+++ Src/HttpDisassembler/HttpDisassembler.Component.cs
@@ -5,24 +5,31 @@ namespace BizTalkComponents.PipelineComponents.HttpDisassembler
 {
     public partial class HttpDisassembler
     {
-        public string Name { get; private set; }
-        public string Version { get; private set; }
-        public string Description { get; private set; }
+        public string Name { get { return "HttpDisassembler"; } }
+        public string Version { get { return "1.0"; } }
+        public string Description
+        {
+            get
+            {
+                return
+                    "Creates message body from URI properties.";
+            }
+        }
         public void GetClassID(out Guid classID)
         {
-            throw new NotImplementedException();
+            classID = new Guid("BF843732-8C30-4E26-BE35-293C763C3EA1");
         }
 
         public void InitNew()
         {
-            throw new NotImplementedException();
+            
         }
 
         public IEnumerator Validate(object projectSystem)
         {
-            throw new NotImplementedException();
+            return null;
         }
 
-        public IntPtr Icon { get; private set; }
+        public IntPtr Icon { get { return IntPtr.Zero; } }
     }
 }
