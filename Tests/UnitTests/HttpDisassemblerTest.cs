11c59a388d767fb3dae8fafe57111c0c9ae761d6
robinhultman <robin.hultman@gmail.com>
2016-03-30 12:52:50 +0200

    Updated test to test both with and without a message body

--- Tests/UnitTests/HttpDisassemblerTest.cs
+++ Tests/UnitTests/HttpDisassemblerTest.cs
@@ -30,11 +30,43 @@ namespace BizTalkComponents.HttpDisassembler.Tests.UnitTests
             var result = pipeline.Execute(message);
 
             Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, result[0].PartCount);
 
             var doc = XDocument.Load(result[0].BodyPart.GetOriginalDataStream());
 
             Assert.AreEqual("value1", doc.Descendants("TestElement1").Single().Value);
             Assert.AreEqual("value2", doc.Descendants("TestElement2").Single().Value);
+        }
+
+        [TestMethod]
+        public void CreateMessageWithBodyTest()
+        {
+            var pipeline = PipelineFactory.CreateEmptyReceivePipeline();
+
+            pipeline.AddDocSpec(typeof(TestSchema));
+
+            var disassembler = new PipelineComponents.HttpDisassembler.HttpDisassembler
+            {
+                DocumentSpecName = "BizTalkComponents.HttpDisassembler.Tests.UnitTests.TestSchema"
+            };
+            string testMessage = "<testmessage></testmessage>";
+            var message = MessageHelper.CreateFromString(testMessage);
+            message.Context.Promote(new ContextProperty("http://BiztalkComponents.PropertySchema#TestProperty1"), "value1");
+            message.Context.Promote(new ContextProperty("http://BiztalkComponents.PropertySchema#TestProperty2"), "value2");
+
+            pipeline.AddComponent(disassembler, PipelineStage.Disassemble);
+
+            var result = pipeline.Execute(message);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(2, result[0].PartCount);
+
+            var doc = XDocument.Load(result[0].GetPart("querypart").GetOriginalDataStream());
+            var bodyDoc = XDocument.Load(result[0].BodyPart.GetOriginalDataStream());
+
+            Assert.AreEqual("value1", doc.Descendants("TestElement1").Single().Value);
+            Assert.AreEqual("value2", doc.Descendants("TestElement2").Single().Value);
+            Assert.AreEqual(testMessage, bodyDoc.ToString());
         }
     }
 }
